f61ef51c3401f2b83af490d0eb4b423429a27b25
Lajaneczka <aleks.malecka1990@gmail.com>
2021-11-15 14:16:14 +0100

    passing props to the Square component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const Square = ({value}) =>{
+  
+    return (
+      <button className="square">
+        {value}
+      </button>
+    );
+  
 }
 
-export default App;
+const Board = () => {
+  
+  const renderSquare = (i) => {
+    return <Square value={i} />;
+  }
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+}
+
+export const Game = () => {
+  
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  
+}
+
+
